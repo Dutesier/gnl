094b77a0756ec469f2ead00d7b9e09d2bca38160
Duarte Areias dos reis <dareias-@c2r2s8.42lisboa.com>
2021-10-29 10:46:17 +0100

    Fixes ft_strjoin

--- get_next_line.c
+++ get_next_line.c
@@ -38,8 +38,13 @@ char *ft_strjoin(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+
 	s3 = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!s3)
 		return (NULL);
